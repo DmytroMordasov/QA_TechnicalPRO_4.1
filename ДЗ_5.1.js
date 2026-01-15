00aa62d5c2cee7b0168f540ebbf8586c6ed92077
Dmytro <dimamordasov107@gmail.com>
2026-01-15 14:34:08 -0500

    ДЗ_5.1

---
+++ ДЗ_5.1.js
@@ -0,0 +1,9 @@
+function pow(x, y) {
+    return x ** y;     //- зведення в ступінь (**) "перше це число яке ми будемо зводити у ступінь, друге у яку ступінь."
+  }
+
+
+  console.log(pow(5, 4));
+  console.log(pow(8, 0));
+  console.log(pow(1, 7));
+  console.log(pow(2, 2));
